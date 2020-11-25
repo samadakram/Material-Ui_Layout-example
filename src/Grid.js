d561ddde4e2fd6d9a7677c14f290bed24caf0533
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-26 04:13:40 +0500

    Responsive Grid

--- src/Grid.js
+++ src/Grid.js
@@ -1,13 +1,55 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
-const GridLayout = ()=>{
-    return(
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    firstRow: {
+        backgroundColor: 'lightBlue',
+    },
+    secondRow: {
+        backgroundColor: 'yellow',
+    },
+    secondRow2: {
+        backgroundColor: 'royalBlue',
+    },
+    thirdRow: {
+        backgroundColor: 'gray',
+    },
+    thirdRow2: {
+        backgroundColor: 'khaki',
+    },
+    thirdRow3: {
+        backgroundColor: 'pink',
+    },
+}));
+
+const GridLayout = () => {
+
+    const classes = useStyles();
+
+    return (
         <div>
             <Grid container>
-                <Grid item>
-                    Hello World !
+                <Grid item xs={12} className={classes.firstRow} >
+                    Hello World 1
+                </Grid>
+                <Grid item xs={12} md={6} className={classes.secondRow} >
+                    Hello World 2
+                </Grid>
+                <Grid item xs={12} md={6} className={classes.secondRow2} >
+                    Hello World 3
+                </Grid>
+                <Grid item xs={6} md={4} className={classes.thirdRow} >
+                    Hello World 4
+                </Grid>
+                <Grid item xs={6} md={4} className={classes.thirdRow2} >
+                    Hello World 5
+                </Grid>
+                <Grid item xs={12} md={4} className={classes.thirdRow3} >
+                    Hello World 6
                 </Grid>
             </Grid>
         </div>
