2e03b372db19450edfa5fa2b3cefda7962942835
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-26 03:56:25 +0500

    Created Grid component

---
+++ src/Grid.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const GridLayout = ()=>{
+    return(
+        <div>
+            <Grid container>
+                <Grid item>
+                    Hello World !
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default GridLayout;
