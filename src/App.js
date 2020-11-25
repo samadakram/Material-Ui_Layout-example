9196d555289df15d09e115b57cc315963bce9ba7
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-26 03:56:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import GridLayout from './Grid';
 
 function App() {
   return (
     <div>
-      Hello World !
+      <GridLayout />
     </div>
   );
 }
