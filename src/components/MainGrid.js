a31432f1a30ded0c6c472ff4e9789e44cf2ee968
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-27 03:11:42 +0500

    Updated MainGrid

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -4,43 +4,33 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
 }));
 
 export default function MainGrid() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>
+                        Global Data
+              </Paper>
+                </Grid>
+                <Grid item xs={8}>
+                    <Paper className={classes.paper}>
+                        Country Data
+          </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
