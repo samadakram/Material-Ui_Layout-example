faf020ac035b19559cbb06442ca38132caaa4f83
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-27 03:06:45 +0500

    Updated App.js
    imported MainGrid in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 // Import Components
 import NavBar from './components/NavBar';
+import MainGrid from './components/MainGrid';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <MainGrid />
     </div>
   );
 }
