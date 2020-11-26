d25077440ae9b0cbcf51adec591fdda9bac1fcaf
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-26 05:07:43 +0500

    Updated RepoPage.js
    Fetched Api for printing Github repos

--- src/RepoPage.js
+++ src/RepoPage.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(1),
+            width: theme.spacing(50),
+            minHeight: theme.spacing(50),
+        },
+
     },
-  },
 }));
 
 export default function SimplePaper() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Paper elevation={3} />
-    </div>
-  );
+    const [repos, setRepos] = useState([{}]);
+
+    useEffect(() => {
+        async function fetchRepos() {
+            const response = await fetch("https://api.github.com/users/samadakram/repos");
+            const data = await response.json();
+            setRepos(data);
+        };
+        fetchRepos();
+    });
+
+    return (
+        <div className={classes.root}>
+            <Paper elevation={3} className={classes.paper}>
+                <h2>My Github Repos</h2>
+                <ol>
+                    {repos.map((repoObj, ind) => {
+                        return (
+                            <li key={ind}>{repoObj.name}</li>
+                        )
+                    })}
+                </ol>
+            </Paper>
+        </div>
+    );
 }
