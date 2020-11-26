390eae8dce62270a1fb9a0db92adc788fd252561
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-27 02:55:57 +0500

    Update Grid.js
    Removed NavBar from Grid .js

--- src/Grid.js
+++ src/Grid.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 // Import Components
-import NavBar from './NavBar';
 import Paper from './RepoPage';
 
 const useStyles = makeStyles((theme) => ({
@@ -29,7 +28,7 @@ const GridLayout = () => {
         <div>
             <Grid container>
                 <Grid item xs={12} className={classes.firstRow} >
-                    <NavBar />
+            
                 </Grid>
                 <Grid item xs={12} className={classes.secondRow} >
                     <Paper />
